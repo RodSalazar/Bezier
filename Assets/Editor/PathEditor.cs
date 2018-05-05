9a238488e30cf852720efc86c5abf9fd2e517d94
Rodolfo <rodolfo.silva@aquiris.com.br>
2018-05-04 21:31:21 -0300

    added repo

---
+++ Assets/Editor/PathEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor {
+
+	PathCreator creator;
+
+	void OnSceneGUI()
+	{
+		Draw();
+	}
+
+	void Draw()
+	{
+		Handles.FreeMoveHandle(Vector3.zero, Quaternion.identity, 0.1f, Vector2.zero, Handles.CylinderHandleCap);
+	}
+	
+	void OnEnable()
+	{
+		creator = (PathCreator)target;
+	}
+}
---
+++ Assets/Path.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path {
+
+	[SerializeField]
+	List<Vector3> points;
+
+	public Path(Vector3 center)
+	{
+		points = new List<Vector3>
+		{
+			center + Vector3.left,
+			center + (Vector3.left + Vector3.up) * 0.5f,
+			center + (Vector3.right + Vector3.down) * 0.5f,
+			center + Vector3.right
+		};
+	}
+
+	public void AddSegment(Vector3 anchorPosition)
+	{
+		points.Add(points[points.Count - 1] * 2 - points[points.Count - 2]);
+		points.Add((points[points.Count - 1] + anchorPosition) * 0.5f);
+		points.Add(anchorPosition);
+	}
+
+	public Vector3[] GetPointsInSegment(int i)
+	{
+		return new Vector3[]{ points[i * 3], }
+	}
+}
