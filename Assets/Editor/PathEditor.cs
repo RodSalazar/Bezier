8fd0fcb2fd2aeac839abe051439434eb4ad2ef23
Rodolfo <rodolfo.silva@aquiris.com.br>
2018-05-07 13:52:36 -0300

    basic bezier functionality in

--- Assets/Editor/PathEditor.cs
+++ Assets/Editor/PathEditor.cs
@@ -9,20 +9,36 @@ public class PathEditor : Editor {
 	PathCreator creator;
 	Path path;
 	private Vector3 pos = Vector3.zero; 
-	public Texture image = Resources.Load("test.png") as Texture;
+	private Texture2D aaTex;
 
 	void OnSceneGUI()
 	{
-		Handles.Label(pos,image);
 		Draw();
+	}
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
 	}
 
 	void Draw()
 	{
-		Handles.color = Color.red;
+		for (int i = 0; i < path.numSegments; i++)
+		{
+			Vector3[] points = path.GetPointsInSegment(i);
+			Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.white, aaTex, 2.5f);
+
+			Handles.color = Color.gray;
+			Handles.DrawDottedLine(points[1],points[0], 3);
+			Handles.DrawDottedLine(points[2],points[3], 3);
+		}
+
+		Handles.color = Color.black;
 		for (int i = 0; i < path.numPoints; i++)
 		{
-			Vector3 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, 0.1f, Vector3.zero, Handles.DotHandleCap);
+			Vector3 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, 0.1f, Vector3.zero, Handles.SphereHandleCap);
+
 			if (path[i] != newPos)
 			{
 				Undo.RecordObject(creator, "Move point");
@@ -34,6 +50,7 @@ public class PathEditor : Editor {
 	
 	void OnEnable()
 	{
+		aaTex = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/EditorTextures/line.png", typeof(Texture2D));
 		creator = (PathCreator)target;
 		if (creator.path == null) creator.CreatePath();
 		path = creator.path;
--- Assets/PathCreator.cs
+++ Assets/PathCreator.cs
@@ -6,8 +6,12 @@ public class PathCreator : MonoBehaviour {
 
 	[HideInInspector]
 	public Path path;
+	
+	public List<Vector3> points;
+	
 	public void CreatePath()
 	{
 		path = new Path(transform.position);
+		
 	}
 }
